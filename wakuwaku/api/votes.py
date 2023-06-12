5fdbbf4cacfb01bcb64f39798f8b2c6c990606d7
shawn_sheep <shawnxu_0227@163.com>
2023-06-12 19:36:28 +0800

    modified:   wakuwaku/api/votes.py

--- wakuwaku/api/votes.py
+++ wakuwaku/api/votes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from wakuwaku.api import bp
-from wakuwaku.models import Post, db
+from wakuwaku.models import Post, db, Vote, Account
 
 @bp.route("/vote", methods=["POST"])
 def vote():
@@ -16,6 +16,11 @@ def vote():
         type: integer
         required: true
         description: The ID of the post to vote on.
+      - name: user_id
+        in: formData
+        type: integer
+        required: true
+        description: The ID of the user casting the vote.
       - name: vote
         in: formData
         type: string
@@ -47,18 +52,25 @@ def vote():
               example: Vote failed
     """
     post_id = request.form.get("post_id")
+    user_id = request.form.get("user_id")
     vote_type = request.form.get("vote")
 
     post = Post.query.get(post_id)
-    if not post:
-        return jsonify({"message": "Post not found"}), 404
+    user = Account.query.get(user_id)
+    if not post or not user:
+        return jsonify({"message": "Post or user not found"}), 404
+
+    vote = Vote.query.filter_by(user_id=user_id, post_id=post_id).first()
+    if vote:
+        return jsonify({"message": "You have already voted on this post"}), 400
+
+    vote = Vote(user_id=user_id, post_id=post_id, vote=vote_type)
+    db.session.add(vote)
 
     if vote_type == "up":
         post.votes += 1
     elif vote_type == "down":
         post.votes -= 1
-    else:
-        return jsonify({"message": "Invalid vote type"}), 400
 
     db.session.commit()
 
